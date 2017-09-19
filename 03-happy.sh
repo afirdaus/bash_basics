57332ee7f13c20139ffa4d76bb7a43c5006c832d
afeefa <afirdaus@ggc.edu>
2017-09-19 18:30:10 -0400

    Changed the file

--- 03-happy.sh
+++ 03-happy.sh
@@ -14,3 +14,11 @@ fi
 
 # exercise: write a script that prints whether today is
 # the weekend or not
+echo "What day is it?"
+read answer2
+
+if [ "$answer2" = "Monday" ]; then
+echo "It is not the weekend :("
+else
+echo "It is the weekend!"
+fi
